3a512c0480ef15f31b19645a2532d991731b3c39
“Matias <matias.n.maidana@gmail.com>
2023-12-12 08:41:19 -0300

    Arreglando problema con el map

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -13,16 +13,17 @@ export default function Album() {
             .then(data => setDatos(data.results))
     }, []);
 
-    //console.log(datos.map(objeto => objeto.id));
-
-    const characters = datos.map(character => <Card key={character.id} name={character.name} imagen={character.image}/>)
+    
 
   return (
     <div className="album py-5 bg-body-tertiary">
       <div className="container">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
-          {characters}
+          
+          {characterData.map((character) => (
+                        <Card key={character.id} image={character.image} name={character.name} />
+                    ))}
         </div>
       </div>
     </div>
