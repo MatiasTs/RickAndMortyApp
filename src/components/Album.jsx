c44650818a894173d5b1d58fb8c3868e649d38c9
“Matias <matias.n.maidana@gmail.com>
2023-12-12 08:37:24 -0300

    Añadiendo props

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -13,23 +13,16 @@ export default function Album() {
             .then(data => setDatos(data.results))
     }, []);
 
-    console.log("*********************1****************")
-
     //console.log(datos.map(objeto => objeto.id));
+
+    const characters = datos.map(character => <Card key={character.id} name={character.name} imagen={character.image}/>)
 
   return (
     <div className="album py-5 bg-body-tertiary">
       <div className="container">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+
+          {characters}
         </div>
       </div>
     </div>
